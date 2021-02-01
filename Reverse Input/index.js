df6c8640520b9fcd09a579e042b88ceb3e4279b2
Tibor Gazsovics <gt008@freemail.hu>
2021-02-01 16:22:26 +0000

    added reset input field button

--- Reverse Input/index.js
+++ Reverse Input/index.js
@@ -1,7 +1,11 @@
 let input = document.getElementById("input");
 let btn = document.getElementById("button");
 let space = document.getElementById("empty-space");
+let resetBtn = document.getElementById("reset-btn");
 
 btn.addEventListener("click", () => {
     space.innerHTML = input.value.split("").reverse().join("");
+});
+resetBtn.addEventListener("click", () => {
+    input.value = "";
 });
